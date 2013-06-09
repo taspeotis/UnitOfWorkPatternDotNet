99d8985374f332ff1116d93b5d720a22f39aa332
Todd Aspeotis <t@speot.is>
2013-06-09 12:15:52 +1000

    Initial work on UnitOfWorkPatternDotNet and NuGet package

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ UnitOfWorkPatternDotNet
 =======================
 
 An (extremely) lightweight UoW interface.
+
+## License
+
+*UnitOfWorkPatternDotNet* is licensed under the [*Microsoft Public License (MS-PL)*](http://www.microsoft.com/en-us/openness/licenses.aspx).
---
+++ UnitOfWorkPatternDotNet/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UnitOfWorkPatternDotNet
+{
+    /// <summary>
+    ///     Represents a unit of work.
+    /// </summary>
+    public interface IUnitOfWork : IDisposable
+    {
+        /// <summary>
+        ///     Commits the unit of work.
+        /// </summary>
+        /// <remarks>
+        ///     Call <see cref="IDisposable.Dispose">Dispose</see> to abandon the unit of work.
+        /// </remarks>
+        void Commit();
+    }
+}
